11d524d129f4904f521f65147f5e5797cecbc634
Josh Marinacci <joshua@marinacci.org>
2015-04-20 21:05:29 -0700

    parse the real expression

--- ui/EditPanel.jsx
+++ ui/EditPanel.jsx
@@ -2,15 +2,21 @@ var React = require('react');
 var Literals = require('../src/Literals');
 var Arithmetic = require('../src/Arithmetic');
 var Expressions = require('../src/Expressions');
-
+var ometajs = require('ometa-js');
+var Parser = require('../parser_compiled.js').Parser;
+/*
 var funcall1 = Expressions.makeFunctionCall(Arithmetic.Add,
     [Literals.makeNumber(5), Literals.makeNumber(5)],
     {});
 var funcall2 = Expressions.makeFunctionCall(Arithmetic.Add,
     [Literals.makeNumber(10),Literals.makeNumber(11)],
     {});
+*/
 function ParseExpression(str) {
-    return funcall1;
+    console.log("parsing expression",str);
+    var out = Parser.matchAll(str,'start');
+    console.log("parsed = " + out.toCode());
+    return out;
 }
 
 
